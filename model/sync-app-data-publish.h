11a0ad4f64a3c325ae23684f3d610b4a83f8b828
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2012-03-05 22:28:33 -0800

    add 3 classes

--- model/sync-app-data-publish.h
+++ model/sync-app-data-publish.h
@@ -22,19 +22,9 @@
 
 #ifndef SYNC_STATE_H
 #define SYNC_STATE_H
-
-extern "C" {
-#include <ccn/ccn.h>
-#include <ccn/charbuf.h>
-#include <ccn/keystore.h>
-#include <ccn/uri.h>
-#include <ccn/bloom.h>
-}
-
-#include <boost/thread.hpp>
 #include <boost/shared_ptr.hpp>
-#include <string>
-#include "sync-data-buffer.h"
+#include "sync-ccnx-wrapper.h"
+
 /**
  * \defgroup sync SYNC protocol
  *
@@ -44,41 +34,18 @@ namespace Sync {
 
 /**
  * \ingroup sync
- * @brief A wrapper for ccnx library; clients of this code do not need to deal
- * with ccnx library
+ * @brief publishes application data and keeps track of most recently published
+ * data
  */
-class CcnxWrapper { 
-private:
-	ccn* m_handle;
-	ccn_keystore *m_keyStore;
-	ccn_charbuf *m_keyLoactor;
-	boost::mutex m_mutex;
-	boost::shared_ptr<boos::thread> m_thread;
-
-private:
-	void createKeyLocator();
-	void initKeyStore();
-	const ccn_pkey *getPrivateKey();
-	const unsigned char *getPublicKeyDigest();
-	ssize_t getPublicKeyDigestLength();
-	void ccnLoop();
-
+class AppDataPublish
+{
 public:
+	std::pair<std::string, boost::shared_ptr<const DataBuffer> > getRecentData();
+	bool publishData(std::string name, const DataBufer &dataBuffer);
   
-
-   CcnxWrapper();
-   ~CcnxWrapper();
-  /**
-   * @brief send Interest 
-   *
-   * @param strInterest the Interest name
-   * @param callback the callback function to deal with the returned data
-   */
-   int sendInterest(std::string strInterest, boost::function<void (DataBuffer &)> processData);
-   int sendInterestFilter(std::string prefix, boost::function<void (std::string)>
-   processInterest);
-   int publishData(std::string name, DataBuffer &dataBuffer);
-
+private:
+	boost::shared_ptr<CcnxWrapper> ccnxHandle;
+	std::pair<std::string, boost::shared_ptr<DataBuffer> > recentData;
 };
 
 } // Sync
--- model/sync-data-buffer.h
+++ model/sync-data-buffer.h
@@ -23,18 +23,12 @@
 #ifndef SYNC_STATE_H
 #define SYNC_STATE_H
 
-extern "C" {
-#include <ccn/ccn.h>
-#include <ccn/charbuf.h>
-#include <ccn/keystore.h>
-#include <ccn/uri.h>
-#include <ccn/bloom.h>
-}
-
-#include <boost/thread.hpp>
+#include <tinyxml.h>
 #include <boost/shared_ptr.hpp>
-#include <string>
-#include "sync-data-buffer.h"
+#include "sync-diff-state.h"
+#include "sync-full-state.h"
+
+
 /**
  * \defgroup sync SYNC protocol
  *
@@ -44,42 +38,35 @@ namespace Sync {
 
 /**
  * \ingroup sync
- * @brief A wrapper for ccnx library; clients of this code do not need to deal
- * with ccnx library
+ * @brief handles conversion from xml encoded data ("wire_format") to states
+ * data (FullState or DiffState)
  */
-class CcnxWrapper { 
+/**
+ * \ingroup sync
+ * @brief Data wrapper with size; convert from/to FullState and DiffState
+ */
+class DataBuffer {
 private:
-	ccn* m_handle;
-	ccn_keystore *m_keyStore;
-	ccn_charbuf *m_keyLoactor;
-	boost::mutex m_mutex;
-	boost::shared_ptr<boos::thread> m_thread;
-
-private:
-	void createKeyLocator();
-	void initKeyStore();
-	const ccn_pkey *getPrivateKey();
-	const unsigned char *getPublicKeyDigest();
-	ssize_t getPublicKeyDigestLength();
-	void ccnLoop();
-
+	unsigned char *m_buffer;
+	size_t m_len;
 public:
-  
+	DataBuffer() {m_buffer = NULL; m_len = 0;}
+	DataBuffer(const unsigned char *buffer, size_t len);
+	DataBuffer(const DataBuffer &dataBuffer);
+	DataBuffer &operator=(const DataBuffer &dataBuffer);
+	~DataBuffer();	
+	size_t length() {return len;}
+	const unsigned char *buffer() { return const_cast<const unsigned char *> (buffer); }
 
-   CcnxWrapper();
-   ~CcnxWrapper();
-  /**
-   * @brief send Interest 
-   *
-   * @param strInterest the Interest name
-   * @param callback the callback function to deal with the returned data
-   */
-   int sendInterest(std::string strInterest, boost::function<void (DataBuffer &)> processData);
-   int sendInterestFilter(std::string prefix, boost::function<void (std::string)>
-   processInterest);
-   int publishData(std::string name, DataBuffer &dataBuffer);
+	DataBuffer &operator<<(FullState &fs);
+	DataBuffer &operator<<(DiffState &ds);
 
+	FullState &operator>>(FullState &fs);
+	DiffState &operator>>(DiffState &ds);
 };
+
+
+
 
 } // Sync
 
