3e1eb16744e7ab9e58279144b36f8d7e1c228707
Chaoyi Bian <bcy@pku.edu.cn>
2012-04-03 16:59:32 -0700

    why include my chinese name in all files.. removed

--- model/sync-full-leaf.h
+++ model/sync-full-leaf.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Zhenkai Zhu <zhenkai@cs.ucla.edu>
- *         卞超轶 Chaoyi Bian <bcy@pku.edu.cn>
+ *         Chaoyi Bian <bcy@pku.edu.cn>
  *	   Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
